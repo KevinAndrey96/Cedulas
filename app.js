beeae135d5fd72d48ba136fd871258649d7bd235
Kevin Andrey Herrera Silva <kahs_kevin@hotmail.com>
2020-04-14 11:11:14 -0500

    CORS

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ var express = require('express');
 const { Person } = require('./models')
 var app = express();
 app.use(express.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 app.post('/', function (req, res) {
 	
 	var messa="";
